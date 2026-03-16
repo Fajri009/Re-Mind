8871515d54405e55a340258ef1b24e5fdf55658e
Fajri009 <102470811+Fajri009@users.noreply.github.com>
2026-03-16 17:00:47 +0700

    fo(feat) - implement application theme with colors, text styles, and icons

--- app/src/main/java/com/example/re_mind/ui/theme/Color.kt
+++ app/src/main/java/com/example/re_mind/ui/theme/Color.kt
@@ -1,11 +1,255 @@
 package com.example.re_mind.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+class MtcAppTheme {
+    class Color {
+        companion object {
+            // Primary
+            val Primary50 = Color(0xFF9F85ED)
+            val Primary300 = Color(0xFF7F5CE7)
+            val Primary500 = Color(0xFF5F33E1)
+            val Primary700 = Color(0xFF4C29B4)
+            val Primary900 = Color(0xFF391F87)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+            // Secondary
+            val Secondary50 = Color(0xFFC1EBFF)
+            val Secondary300 = Color(0xFFACE5FF)
+            val Secondary500 = Color(0xFF97DEFF)
+            val Secondary700 = Color(0xFF88C8E6)
+            val Secondary900 = Color(0xFF79B2CC)
+
+            // Neutral
+            val Neutral50 = Color(0xFFF3F1FF)
+            val Neutral100 = Color(0xFFEAE7ED)
+            val Neutral200 = Color(0xFFD0D0D0)
+            val Neutral300 = Color(0xFFBDBDBD)
+            val Neutral400 = Color(0xFFAAAAAA)
+            val Neutral500 = Color(0xFF909090)
+
+            // Success
+            val Success50 = Color(0xFFAFFCB6)
+            val Success300 = Color(0xFF94FB9E)
+            val Success500 = Color(0xFF79FA86)
+            val Success700 = Color(0xFF61C86B)
+            val Success900 = Color(0xFF499650)
+
+            // Warning
+            val Warning50 = Color(0xFFFFE090)
+            val Warning300 = Color(0xFFFFD56B)
+            val Warning500 = Color(0xFFFFD059)
+            val Warning700 = Color(0xFFFFCB46)
+            val Warning900 = Color(0xFFE6B73F)
+
+            // Error
+            val Error50 = Color(0xFFE56969)
+            val Error300 = Color(0xFFE15757)
+            val Error500 = Color(0xFFDE4444)
+            val Error700 = Color(0xFFC83D3D)
+            val Error900 = Color(0xFFB23636)
+
+            // Shades
+            val Shades50 = Color(0xFFF6F6F6)
+            val Shades300 = Color(0xFF101010)
+        }
+    }
+
+    class Text {
+        companion object {
+            // Normal
+            val heading1 =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 53.sp
+                )
+
+            val heading2 =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 43.sp
+                )
+
+            val heading3 =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 34.sp
+                )
+
+            val heading4 =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 27.sp
+                )
+
+            val heading5 =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 22.sp
+                )
+
+            val heading6 =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 18.sp
+                )
+
+            val paragraph1 =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 14.sp
+                )
+
+            val paragraph2 =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 12.sp
+                )
+
+            val paragraph3 =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 10.sp
+                )
+
+            // SemiBold
+            val heading1SemiBold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 53.sp
+                )
+
+            val heading2SemiBold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 43.sp
+                )
+
+            val heading3SemiBold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 34.sp
+                )
+
+            val heading4SemiBold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 27.sp
+                )
+
+            val heading5SemiBold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 22.sp
+                )
+
+            val heading6SemiBold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 18.sp
+                )
+
+            val paragraph1SemiBold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 14.sp
+                )
+
+            val paragraph2SemiBold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 12.sp
+                )
+
+            val paragraph3SemiBold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 10.sp
+                )
+
+            // Bold
+            val heading1Bold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 53.sp
+                )
+
+            val heading2Bold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 43.sp
+                )
+
+            val heading3Bold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 34.sp
+                )
+
+            val heading4Bold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 27.sp
+                )
+
+            val heading5Bold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 22.sp
+                )
+
+            val heading6Bold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp
+                )
+
+            val paragraph1Bold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp
+                )
+
+            val paragraph2Bold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 12.sp
+                )
+
+            val paragraph3Bold =
+                TextStyle(
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 10.sp
+                )
+        }
+    }
+}
--- app/src/main/java/com/example/re_mind/ui/theme/Theme.kt
+++ app/src/main/java/com/example/re_mind/ui/theme/Theme.kt
@@ -1,37 +1,36 @@
 package com.example.re_mind.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.example.re_mind.R
+import com.example.re_mind.ui.theme.MtcAppTheme.Color.Companion.Primary500
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Primary500
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Primary500
 )
+
+val poppinsFontFamily =
+    FontFamily(
+        Font(R.font.poppins_thin, FontWeight.Light),
+        Font(R.font.poppins_regular, FontWeight.Normal),
+        Font(R.font.poppins_italic, FontWeight.Normal, FontStyle.Italic),
+        Font(R.font.poppins_medium, FontWeight.Medium),
+        Font(R.font.poppins_bold, FontWeight.Bold)
+    )
 
 @Composable
 fun ReMindTheme(
@@ -43,7 +42,7 @@ fun ReMindTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (darkTheme) dynamicLightColorScheme(context) else dynamicLightColorScheme(context)
         }
 
         darkTheme -> DarkColorScheme
